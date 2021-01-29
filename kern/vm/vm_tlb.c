fa0dd75fc8ba84ca9c1e6393a89fcd1b8701999b
enzo-bisceglia <65443459+enzo-bisceglia@users.noreply.github.com>
2021-01-29 16:31:27 +0100

    Update vm_tlb.c

--- kern/vm/vm_tlb.c
+++ kern/vm/vm_tlb.c
@@ -11,6 +11,9 @@ static struct spinlock tlb_lock = SPINLOCK_INITIALIZER;
 int tlb_ff;
 int tlb_fr;
 
+/*
+alloca la bitmap (reale), inizializza con valori nulli
+*/
 int
 tlb_map_init(void) {
 
@@ -27,6 +30,9 @@ tlb_map_init(void) {
     return 0;
 }
 
+/*
+seleziona una vittima all' interno della tlb applicando logica round robin
+*/
 static
 int
 tlb_get_rr_victim(void){
@@ -37,6 +43,9 @@ tlb_get_rr_victim(void){
     return victim;
 }
 
+/*
+ricerca un bit libero all' interno della bitmap e restituisce l'indice relativo in TLB. 
+*/
 static
 int
 tlb_get_fresh_index(void){
@@ -45,12 +54,12 @@ tlb_get_fresh_index(void){
 
 	spinlock_acquire(&tlb_lock);
 	for (i=0; i<TLB_MAP_SIZE; i++){
-		if ((tlb_map[i]&0xff)==0xff)
+		if ((tlb_map[i]&0xff)==0xff) //guardo il byte, se c'è anche un solo bit libero fermo la ricerca
 			continue;
 		for (j=0; j<8; j++){
 			if (((~tlb_map[i])&ix)==ix){
                 spinlock_release(&tlb_lock);
-				return (8*i)+j;
+				return (8*i)+j; //restituisco l'indice
 			}
 			else
 				ix<<=1;
@@ -60,6 +69,12 @@ tlb_get_fresh_index(void){
     return -1;
 }
 
+/*
+scrive la entry corrispondente nella tlb, l' indice può essere già passato dalla funzione chiamante
+(e.g. perché si sta "rimpiazzando" una entry la cui pagina sta per essere swappata fuori dalla ram)
+oppure ricercato all' interno della bitmap. Se non c'è nemmeno un' entry libera ne verrà sacrificata
+un' altra secondo la tecnica round robin. Aggiorna la bitmap.
+*/
 void
 tlb_write_entry(int* index, uint32_t ehi, uint32_t elo){
 
@@ -85,6 +100,10 @@ tlb_write_entry(int* index, uint32_t ehi, uint32_t elo){
     splx(spl);
 }
 
+
+/*
+invalida una entry della tlb. Aggiorna la bitmap.
+*/
 void
 tlb_clean_entry(int index){
 
